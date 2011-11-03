3cf2c7b446d446c0feca2940927c9dc26f0dda30
Michael Sloan <mgsloan@gmail.com>
2011-11-03 16:47:53 -0700

    Oops, forgot new classes

---
+++ KinectViewer/KinectViewer/MotionRecord.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Xna.Framework;
+
+namespace KinectViewer
+{
+    
+class MotionRecord
+{
+    public LinkedList<Tuple<long, double[]>> data = new LinkedList<Tuple<long, double[]>>();
+
+    public void TakeAngleSample(NaoUpperBody nao, int limit = -1)
+    {
+        Type typ = nao.values[0].GetType();
+        if (typ == typeof(float))
+        {
+            double[] sample = new double[nao.values.Count];
+            for (int i = 0; i < sample.Count(); i++)
+            {
+                sample[i] = (double)(float)nao.values[i];
+            }
+            AddSample(sample, limit);
+        }
+    }
+
+    public void TakePosSample(Vector3 pos, int limit = -1)
+    {
+        double[] sample = new double[3];
+        sample[0] = pos.X; sample[1] = pos.Y; sample[2] = pos.Z;
+        AddSample(sample, limit);
+    }
+
+    public void AddSample(double[] sample, int limit = -1)
+    {
+        data.AddLast(new Tuple<long, double[]>(DateTime.Now.ToFileTime(), sample));
+        if (limit > 0 && data.Count > limit) data.RemoveFirst();
+    }
+
+    public double[][] GetArray()
+    {
+        double[][] result = new double[data.Count][];
+        int i = 0;
+        foreach (Tuple<long, double[]> tup in data) {
+            result[i] = tup.Item2;
+            i += 1;
+        }
+        return result;
+    }
+
+    public void SaveRecording()
+    {
+        System.IO.Directory.CreateDirectory("saved");
+        string path = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "saved/" + DateTime.Now.ToFileTime().ToString() + ".rec");
+        SaveRecording(path);
+    }
+
+    public void SaveRecording(string name)
+    {
+        StreamWriter writer = new StreamWriter(name);
+        StringBuilder builder = new StringBuilder();
+        builder.AppendFormat("{0}", DateTime.Now.ToFileTime().ToString());
+        foreach (Tuple<long, double[]> tup in data)
+        {
+            builder.Append(tup.Item1);
+            builder.Append(", ");
+            double[] vals = tup.Item2;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if (i != vals.Length) builder.Append(", ");
+                builder.AppendFormat("{0}", vals[i]);
+            }
+            writer.WriteLine(builder.ToString());
+            builder.Clear();
+        }
+    }
+
+    public int TrimMotionless(bool fromBack, int minRem, double sqThresh)
+    {
+        double[] prior = null;
+        int remCount = 0;
+        for (int i = fromBack ? data.Count : 0;
+            fromBack ? i >= 0 : i < data.Count;
+            i += fromBack ? -1 : 1)
+        {
+            double[] vals = data.ElementAt(i).Item2;
+            if (prior != null)
+            {
+                double dist = 0;
+                for (int j = 0; j < vals.Length; j++) {
+                    dist += Math.Pow(vals[j] - prior[j], 2);
+                }
+                if (dist < sqThresh) remCount++; else break;
+            }
+            prior = (double[])vals.Clone();
+        }
+        if (remCount > minRem)
+        {
+            if (fromBack) {
+                for (int i = 0; i < remCount; i++) data.RemoveLast();
+            } else {
+                for (int i = 0; i < remCount; i++) data.RemoveFirst();
+            }
+            return remCount;
+        }
+        return 0;
+    }
+}
+
+}
---
+++ KinectViewer/KinectViewer/SoundController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using Bespoke.Common.Osc;
+
+namespace KinectViewer
+{
+    public class SoundController
+    {
+        IPEndPoint _sound_source = null;
+        IPEndPoint _sound_dest = null;
+        double[] prior = null;
+        double[] speeds = null;
+
+        public SoundController(int dport = 10337, int sport = 10338)
+        {
+            _sound_source = new IPEndPoint(IPAddress.Loopback, sport);
+            _sound_dest = new IPEndPoint(IPAddress.Loopback, dport);
+
+            // Barf
+            OscPacket.LittleEndianByteOrder = false;
+        }
+
+        public void sendRotationSpeeds(double[] values)
+        {
+            if (prior != null)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    speeds[i] = ((float)speeds[i] + (float)values[i] - (float)prior[i]) / 2;
+                    if (float.IsNaN((float)speeds[i]) || float.IsInfinity((float)speeds[i])) speeds[i] = 0f;
+                    prior[i] = values[i];
+                    float val = (Math.Abs((float)speeds[i]) * 10000f);
+                    OscMessage msg = new OscMessage(_sound_source, "/motor" + i.ToString(), val);
+                    msg.Send(_sound_dest);
+                }
+            }
+            else
+            {
+                speeds = new double[values.Length];
+            }
+            prior = (double[]) values.Clone();
+        }
+
+        public void triggerDing()
+        {
+            OscMessage msg = new OscMessage(_sound_source, "/ding", 1);
+            msg.Send(_sound_dest);
+        }
+    }
+}
